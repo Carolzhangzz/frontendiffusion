f5af4c60b8fc4dc2478cdf3eed91ee2dde47b192
carol_zhang <carolzhang705@gmail.com>
2024-07-18 17:43:45 -0700

    Isolate platform folder in separate branch

--- backend/routes/prdRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const prdController = require("../controllers/prdController");
-
-router.post("/generate-prd", prdController.generatePRD);
-
-module.exports = router;
